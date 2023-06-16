6348793844397654085e329792cc4847771bf540
fabriciovo_dws <106166138+fabriciovariscodws@users.noreply.github.com>
2023-06-16 17:20:18 -0300

    add seo stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,37 @@
+import { NextSeo, SiteLinksSearchBoxJsonLd } from "next-seo";
 import Head from "next/head";
+
+const SiteLinksSearchBox = () => (
+  <>
+    <SiteLinksSearchBoxJsonLd
+      url="https://fabriciovariscodws.github.io/next-seo-test/"
+      potentialActions={[
+        {
+          target: 'https://fabriciovariscodws.github.io/next-seo-test/search?q',
+          queryInput: 'search_term_string',
+        },
+        {
+          target: 'android-app://https://fabriciovariscodws.github.io/next-seo-test/search/?q',
+          queryInput: 'search_term_string',
+        },
+      ]}
+    />
+  </>
+);
+
 
 export default function Home() {
   return (
     <>
+      <NextSeo
+        description={'Testing'}
+        title={'Teste SEO'}
+        canonical={'https://fabriciovariscodws.github.io/next-seo-test/'}
+
+      />
+      <Head>
+      <SiteLinksSearchBox />
+      </Head>
       <main>
         <h1>SEO TESTE</h1>
       </main>
---
+++ src/pages/search/[slug].jsx
@@ -0,0 +1,10 @@
+
+export default function Search() {
+  return (
+    <>
+      <main>
+        <h1>aaaaa</h1>
+      </main>
+    </>
+  );
+}
